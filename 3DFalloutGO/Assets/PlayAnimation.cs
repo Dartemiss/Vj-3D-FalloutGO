9be1fd89d6c9bfbaca7040a58318dcca16166dd0
Unknown <nil.mullol@gmail.com>
2018-05-23 19:42:19 +0200

    peaso commit: models, particles, etc

---
+++ 3DFalloutGO/Assets/PlayAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    Animator m_Animator;
+    float old_posX, old_posY, old_posZ;
+    void Start()
+    {
+        old_posX = transform.position.x;
+        old_posY = transform.position.y;
+        old_posZ = transform.position.z;
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (old_posY == transform.position.y) { 
+            if (old_posX != transform.position.x || old_posZ != transform.position.z)
+            {
+                m_Animator.Play("Walk");
+            }
+            else
+            {
+                m_Animator.Play("Idle");
+            }
+            
+        }
+        else
+        {
+
+        }
+        old_posX = transform.position.x;
+        old_posZ = transform.position.z;
+        old_posY = transform.position.y;
+    }
+}
