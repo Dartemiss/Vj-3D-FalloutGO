30c49d92b6b857594036700a8840a5e0e183b72d
Dartemiss <enrique.alexand.gonzalez@est.fib.upc.edu>
2018-05-24 23:59:38 +0200

    construir lvl1 + teleport

---
+++ 3DFalloutGO/Assets/Scrpts/TeleportScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour {
+	public Transform mainCharacter;
+	public Transform whereToGo;
+	float timer = 0.0f;
+	float timerMax = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (mainCharacter.transform.position, transform.position) < 1.0f) {
+			timer += Time.deltaTime;
+			if(timerMax<timer)
+				mainCharacter.transform.position = new Vector3 (whereToGo.position.x, whereToGo.position.y + 0.51f, whereToGo.position.z);
+		}
+	}
+		
+}
