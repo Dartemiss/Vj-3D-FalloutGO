e25996a2fa82c0d1e9567f51d15c29178ceabab6
Unknown <nil.mullol@gmail.com>
2018-05-25 17:34:52 +0200

    new ui things, models tp and button

--- 3DFalloutGO/Assets/Scrpts/EnemyHasBeenShot.cs
+++ 3DFalloutGO/Assets/Scrpts/EnemyHasBeenShot.cs
@@ -19,12 +19,12 @@ public class EnemyHasBeenShot : MonoBehaviour {
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.tag == "my_shot") {
-			Destroy (collision.gameObject, 2);
+			Destroy (collision.gameObject, 1);
             var particles = Instantiate(deathEffect, transform);
-            Destroy(particles.gameObject, 2);
+            Destroy(particles.gameObject, 1);
             GetComponentInChildren<MeshRenderer>().enabled = false;
             GetComponent<BoxCollider>().enabled = false;
-            Destroy (gameObject, 2);
+            Destroy (gameObject, 1);
 
         }
 	}
