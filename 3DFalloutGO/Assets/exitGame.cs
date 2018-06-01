e461b7be3411b0dd3189afbb1e9dd6e374f9ab8e
Unknown <nil.mullol@gmail.com>
2018-06-01 21:04:31 +0200

    exit

---
+++ 3DFalloutGO/Assets/exitGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitGame : MonoBehaviour {
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
